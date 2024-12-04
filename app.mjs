50cfba0348ac63a49de396a83037f6ae38ab1845
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-12-04 15:35:28 +0000

    Implement logging in and out with passport

--- app.mjs
+++ app.mjs
@@ -2,8 +2,12 @@ import express from 'express';
 import session from 'express-session';
 import connectPgSimple from 'connect-pg-simple';
 import pg from 'pg';
-const { Pool } = pg;
+import passport from 'passport';
+import passportLocal from 'passport-local';
 import 'dotenv/config';
+
+const { Pool } = pg;
+const LocalStrategy = passportLocal.Strategy;
 
 const PORT = process.env.PORT;
 
@@ -11,10 +15,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
+// Set up session.
 const pgStore = new connectPgSimple(session);
-
 const pool = new Pool({ connectionString: process.env.DB });
-
 app.use(
   session({
     store: new pgStore({
@@ -27,8 +30,64 @@ app.use(
   }),
 );
 
+// Set up passport and strategy.
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        'SELECT * FROM app_user WHERE username = $1',
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, {
+          message: 'That username does not exist',
+        });
+      }
+
+      if (password !== user.password) {
+        return done(null, false, {
+          message: 'The username and password do not match',
+        });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM app_user WHERE id = $1', [
+      id,
+    ]);
+    const user = rows[0];
+    done(null, {
+      id: user.id,
+      username: user.username,
+    });
+  } catch (error) {
+    done(error);
+  }
+});
+
+app.use(passport.session());
+app.use((req, res, next) => {
+  console.log(' session:', req.session);
+  console.log('    user:', req.user);
+  console.log('messages:', req.session.messages);
+  next();
+});
+
 app.get('/', (req, res, next) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Index', user: req.user });
 });
 
 app.get('/sign-up', (req, res, next) => {
@@ -49,6 +108,21 @@ app.post('/sign-up', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+app.post(
+  '/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  }),
+);
+
+app.post('/log-out', async (req, res, next) => {
+  req.logOut((error) => {
+    if (error) return next(error);
+    res.redirect('/');
+  });
 });
 
 app.use((req, res, next) => {
