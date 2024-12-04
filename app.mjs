09c11a1ebcb24a694f646f9f0cd529c46c6fde20
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-12-04 14:37:30 +0000

    Implement user sign up

--- app.mjs
+++ app.mjs
@@ -8,16 +8,17 @@ import 'dotenv/config';
 const PORT = process.env.PORT;
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
 const pgStore = new connectPgSimple(session);
 
+const pool = new Pool({ connectionString: process.env.DB });
+
 app.use(
   session({
     store: new pgStore({
-      pool: new Pool({
-        connectionString: process.env.DB,
-      }),
+      pool,
     }),
     secret: process.env.SECRET,
     resave: false,
@@ -27,16 +28,35 @@ app.use(
 );
 
 app.get('/', (req, res, next) => {
-  console.log('session:', req.session);
-  res.send('<h1>Hello world</h1>');
+  res.render('index', { title: 'Index' });
+});
+
+app.get('/sign-up', (req, res, next) => {
+  res.render('sign-up', { title: 'Sign up' });
+});
+
+app.post('/sign-up', async (req, res, next) => {
+  try {
+    // TO DO
+    // Will hash password etc. later once general user sign up and login is working.
+    const { username, password } = req.body;
+    const timestamp = new Date(Date.now());
+    await pool.query(
+      'INSERT INTO app_user (username, password, created) VALUES ($1, $2, $3) RETURNING *',
+      [username, password, timestamp],
+    );
+    res.status(201).redirect('/');
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use((req, res, next) => {
-  res.send('<h1>404 - Page not found</h1>');
+  res.status(404).render('404', { title: 'Page not found' });
 });
 
 app.use((error, req, res, next) => {
-  res.send(`<h1>500 - Internal server error</h1><p>${error.message}</p>`);
+  res.status(500).render('error', { title: 'An error has occurred', error });
 });
 
 app.listen(PORT, () => console.log('Server listening on port', PORT));
